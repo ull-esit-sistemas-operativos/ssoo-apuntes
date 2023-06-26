29eff21933ed144f4f95d16bccb26449ec1c752f
Jesús Torres <jmtorres@ull.es>
2023-06-26 10:34:36 +0000

    chore: actualización conf del devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+sudo apt-get -y install --no-install-recommends
+    libxml2-dev libxslt-dev \
+    bison flex libffi-dev libxml2-dev libgdk-pixbuf-2.0-dev libcairo2-dev \
+    libpango1.0-dev libwebp-dev libzstd-dev fonts-lyx cmake \
+    pandoc
+
 bundle config set --local path.system true
 bundle config set --local build.nokogiri --use-system-libraries
 bundle config set --local without epub3
